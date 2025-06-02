3f5f83b5db3456d7e9394c3569d1c4b9b87c0ded
Rhuan Barros <rhuanbarros@gmail.com>
2025-06-02 12:46:37 +0000

    removed old files

--- OLD_PROJECT_FILES_TO_ANALYSE/app/configuration/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import ConfigurationView from '@/components/ConfigurationView';
-
-export default function ConfigurationPage() {
-  return <ConfigurationView />;
-} 
--- OLD_PROJECT_FILES_TO_ANALYSE/app/saved-prompts/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import SavedPromptsView from '@/components/SavedPromptsView';
-
-export default function SavedPromptsPage() {
-  return <SavedPromptsView />;
-} 
